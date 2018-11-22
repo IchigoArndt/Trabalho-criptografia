aeeb0ff1349be917ff7335c97b09a2bc62f52e04
IchigoArndt <arndtfelipe@gmail.com>
2018-11-22 13:00:46 -0200

    commit initial

---
+++ TrabCrip/TrabCrip.Infra/CriptografiaRSA.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+namespace TrabCrip.Infra
+{
+    public class CriptografiaRSA
+    {
+        static public byte[] RSACifra(byte[] byteCifrado, RSAParameters RSAInfo, bool isOAEP)
+        {
+            try
+            {
+                byte[] DadosCifrados;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {   
+                    RSA.ImportParameters(RSAInfo);
+
+                    DadosCifrados = RSA.Encrypt(byteCifrado, isOAEP);
+                }
+                return DadosCifrados;
+            }
+            catch (CryptographicException e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        static public byte[] RSADecifra(byte[] byteDecifrado, RSAParameters RSAInfo, bool isOAEP)
+        {
+            try
+            {
+                byte[] DadosDecifrados;
+                
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    
+                    RSA.ImportParameters(RSAInfo);
+
+                    DadosDecifrados = RSA.Decrypt(byteDecifrado, isOAEP);
+                }
+                return DadosDecifrados;
+            }
+            catch (CryptographicException e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
---
+++ TrabCrip/TrabCrip.apresentacao/MenuPrincipal.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TrabCrip.apresentacao
+{
+    partial class menuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRSA = new System.Windows.Forms.Button();
+            this.btnSHA = new System.Windows.Forms.Button();
+            this.btnRSHA = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRSA
+            // 
+            this.btnRSA.Location = new System.Drawing.Point(39, 3);
+            this.btnRSA.Name = "btnRSA";
+            this.btnRSA.Size = new System.Drawing.Size(101, 23);
+            this.btnRSA.TabIndex = 0;
+            this.btnRSA.Text = "RSA";
+            this.btnRSA.UseVisualStyleBackColor = true;
+            // 
+            // btnSHA
+            // 
+            this.btnSHA.Location = new System.Drawing.Point(39, 71);
+            this.btnSHA.Name = "btnSHA";
+            this.btnSHA.Size = new System.Drawing.Size(101, 23);
+            this.btnSHA.TabIndex = 1;
+            this.btnSHA.Text = "SHA 256";
+            this.btnSHA.UseVisualStyleBackColor = true;
+            // 
+            // btnRSHA
+            // 
+            this.btnRSHA.Location = new System.Drawing.Point(39, 134);
+            this.btnRSHA.Name = "btnRSHA";
+            this.btnRSHA.Size = new System.Drawing.Size(101, 23);
+            this.btnRSHA.TabIndex = 2;
+            this.btnRSHA.Text = "RSA + SHA 256";
+            this.btnRSHA.UseVisualStyleBackColor = true;
+            // 
+            // menuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(190, 159);
+            this.Controls.Add(this.btnRSHA);
+            this.Controls.Add(this.btnSHA);
+            this.Controls.Add(this.btnRSA);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "menuPrincipal";
+            this.Text = "Criptografia";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRSA;
+        private System.Windows.Forms.Button btnSHA;
+        private System.Windows.Forms.Button btnRSHA;
+    }
+}
+
